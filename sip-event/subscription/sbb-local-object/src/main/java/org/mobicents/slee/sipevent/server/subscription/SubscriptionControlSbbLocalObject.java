39517c559e35a3dc85963f58872f52f9bdd2286c
emmartins <emmartins@bf0df8d0-2c1f-0410-b170-bd30377b63dc>
2009-12-11 14:33:09 +0000

    sip presence migration to slee 2.x
    git-svn-id: https://mobicents.googlecode.com/svn/trunk/servers/sip-presence@8945 bf0df8d0-2c1f-0410-b170-bd30377b63dc

--- sip-event/subscription/sbb-local-object/src/main/java/org/mobicents/slee/sipevent/server/subscription/SubscriptionControlSbbLocalObject.java
+++
@@ -1,8 +0,0 @@
-package org.mobicents.slee.sipevent.server.subscription;
-
-
-public interface SubscriptionControlSbbLocalObject extends
-		ImplementedSubscriptionControlParentSbbLocalObject,
-		SubscriptionClientControlSbbLocalObject,EventListSubscriptionControlParentSbbLocalObject {
-
-}
