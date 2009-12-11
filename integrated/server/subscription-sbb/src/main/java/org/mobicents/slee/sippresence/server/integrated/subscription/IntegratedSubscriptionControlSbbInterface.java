b825a6a758f7bf682881558c5fa04ac362fb72bc
emmartins <emmartins@bf0df8d0-2c1f-0410-b170-bd30377b63dc>
2009-12-11 13:02:18 +0000

    sip presence migration to slee 2.x
    git-svn-id: https://mobicents.googlecode.com/svn/trunk/servers/sip-presence@8935 bf0df8d0-2c1f-0410-b170-bd30377b63dc

---
+++ integrated/server/subscription-sbb/src/main/java/org/mobicents/slee/sippresence/server/integrated/subscription/IntegratedSubscriptionControlSbbInterface.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package org.mobicents.slee.sippresence.server.integrated.subscription;
+
+import org.mobicents.slee.sippresence.server.subscription.PresenceSubscriptionControlSbbInterface;
+import org.mobicents.slee.xdm.server.subscription.XcapDiffSubscriptionControlSbbInterface;
+
+/**
+ * @author martins
+ *
+ */
+public interface IntegratedSubscriptionControlSbbInterface extends XcapDiffSubscriptionControlSbbInterface, PresenceSubscriptionControlSbbInterface{
+
+}
