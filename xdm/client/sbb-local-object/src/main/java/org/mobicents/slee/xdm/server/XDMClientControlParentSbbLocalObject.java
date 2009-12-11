455c1f1cc0432fcb440b00ba59eaecdeb95fd26f
emmartins <emmartins@bf0df8d0-2c1f-0410-b170-bd30377b63dc>
2009-12-11 13:52:06 +0000

    sip presence migration to slee 2.x
    git-svn-id: https://mobicents.googlecode.com/svn/trunk/servers/sip-presence@8940 bf0df8d0-2c1f-0410-b170-bd30377b63dc

---
+++ xdm/client/sbb-local-object/src/main/java/org/mobicents/slee/xdm/server/XDMClientControlParentSbbLocalObject.java
@@ -0,0 +1,13 @@
+package org.mobicents.slee.xdm.server;
+
+import javax.slee.SbbLocalObject;
+/**
+ * Interface used by {@link XDMClientControlSbbLocalObject} as callback to the
+ * sbb that declares it as a child sbb.
+ * 
+ * @author martins
+ * 
+ */
+public interface XDMClientControlParentSbbLocalObject extends SbbLocalObject, XDMClientControlParent {
+
+}
