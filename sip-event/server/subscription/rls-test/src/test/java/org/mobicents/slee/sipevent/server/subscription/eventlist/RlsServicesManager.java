317469110cf6917fa0493ffc152fdc95dd5a7ba8
emmartins <emmartins@bf0df8d0-2c1f-0410-b170-bd30377b63dc>
2009-12-11 14:49:28 +0000

    sip presence migration to slee 2.x
    git-svn-id: https://mobicents.googlecode.com/svn/trunk/servers/sip-presence@8949 bf0df8d0-2c1f-0410-b170-bd30377b63dc

--- sip-event/server/subscription/rls-test/src/test/java/org/mobicents/slee/sipevent/server/subscription/eventlist/RlsServicesManager.java
+++ sip-event/server/subscription/rls-test/src/test/java/org/mobicents/slee/sipevent/server/subscription/eventlist/RlsServicesManager.java
@@ -2,9 +2,20 @@ package org.mobicents.slee.sipevent.server.subscription.eventlist;
 
 import java.io.IOException;
 import java.io.StringWriter;
+import java.util.Hashtable;
 
+import javax.management.InstanceNotFoundException;
+import javax.management.MBeanException;
+import javax.management.MalformedObjectNameException;
+import javax.management.ObjectName;
+import javax.management.ReflectionException;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
 import javax.xml.bind.JAXBContext;
 
+import org.jboss.jmx.adaptor.rmi.RMIAdaptor;
+import org.mobicents.slee.enabler.userprofile.jpa.jmx.UserProfileControlManagementMBean;
 import org.openxdm.xcap.client.XCAPClient;
 import org.openxdm.xcap.client.appusage.resourcelists.jaxb.EntryType;
 import org.openxdm.xcap.client.appusage.resourcelists.jaxb.ListType;
@@ -23,19 +34,65 @@ public class RlsServicesManager {
 	private final String serviceUri;
 	private final String[] entryURIs;
 	
+	protected String password = "password";
+	
+	private ObjectName userProfileMBeanObjectName;
+	private RMIAdaptor rmiAdaptor;
+		
+	private void initRmiAdaptor() throws NamingException, MalformedObjectNameException, NullPointerException {
+		// Set Some JNDI Properties
+		Hashtable env = new Hashtable();
+		env.put(Context.PROVIDER_URL, "jnp://"+ServerConfiguration.SERVER_HOST+":1099");
+		env.put(Context.INITIAL_CONTEXT_FACTORY,
+				"org.jnp.interfaces.NamingContextFactory");
+		env.put(Context.URL_PKG_PREFIXES, "org.jnp.interfaces");
+
+		InitialContext ctx = new InitialContext(env);
+		rmiAdaptor = (RMIAdaptor) ctx.lookup("jmx/rmi/RMIAdaptor");
+		userProfileMBeanObjectName = new ObjectName(UserProfileControlManagementMBean.MBEAN_NAME);
+	}
+	
+	private void createUser(String user, String password) throws InstanceNotFoundException, MBeanException, ReflectionException, IOException {
+		String sigs[] = { String.class.getName(), String.class.getName() };
+		Object[] args = { user, password };
+		rmiAdaptor.invoke(userProfileMBeanObjectName, "addUser", args, sigs);	
+	}
+	
+	private void removeUser(String user) throws InstanceNotFoundException, MBeanException, ReflectionException, IOException {
+		String sigs[] = { String.class.getName()};
+		Object[] args = { user};
+		rmiAdaptor.invoke(userProfileMBeanObjectName, "removeUser", args, sigs);	
+	}
+	
+	public void runBefore() throws IOException, InterruptedException, MalformedObjectNameException, NullPointerException, NamingException, InstanceNotFoundException, MBeanException, ReflectionException {
+		xCAPClient = ServerConfiguration.getXCAPClientInstance();	
+		initRmiAdaptor();
+		createUser(serviceUri, password);
+		xCAPClient.setAuthenticationCredentials(serviceUri, password);
+		xCAPClient.setDoAuthentication(true);
+	}
+	
+	public void runAfter() throws IOException, InstanceNotFoundException, MBeanException, ReflectionException {
+		if (xCAPClient != null) {
+			xCAPClient.shutdown();
+			xCAPClient = null;
+		}		
+		removeUser(serviceUri);
+	}
+	
 	public RlsServicesManager(String serviceUri, String[] entryURIs, ResourceListServerSipTest test) {
 		this.serviceUri = serviceUri;
 		this.entryURIs = entryURIs;
-		this.test = test;
-		try {
-			xCAPClient = ServerConfiguration.getXCAPClientInstance();
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-			test.failTest(e.getMessage());
-		}
+		this.test = test;		
 	}
 	
 	public void putRlsServices() {
+		try {
+			runBefore();
+		} catch (Throwable e) {
+			e.printStackTrace();
+			test.failTest(e.getMessage());
+		}
 		try {
 			xCAPClient.put(new UserDocumentUriKey(RLSServicesAppUsage.ID,serviceUri,"index"), RLSServicesAppUsage.MIMETYPE, getRlsServices(entryURIs).getBytes("UTF-8"),null);
 		} catch (Exception e) {
@@ -48,6 +105,12 @@ public class RlsServicesManager {
 		try {
 			xCAPClient.delete(new UserDocumentUriKey(RLSServicesAppUsage.ID,serviceUri,"index"),null);
 		} catch (Exception e) {
+			e.printStackTrace();
+			test.failTest(e.getMessage());
+		}
+		try {
+			runAfter();
+		} catch (Throwable e) {
 			e.printStackTrace();
 			test.failTest(e.getMessage());
 		}
